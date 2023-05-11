d9e1c51fd935d79300c82e83b6d1216de5e00976
MichaeLOl1v <micheeloliv@outlook.com>
2023-05-11 19:39:40 -0300

    Initial commit

---
+++ app/src/main/java/br/com/etecia/adotapetsdois/cad_usu_Activity.java
@@ -0,0 +1,14 @@
+package br.com.etecia.adotapetsdois;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class cad_usu_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cad_usu_layout);
+    }
+}
